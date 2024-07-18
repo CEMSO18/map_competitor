8b9e48dbb9890b15debd9dac25a8c44a3618d05b
CEMSO18 <csoquet@icloud.com>
2024-07-18 10:07:15 +0200

    Create test_check_run_zokeeper.ps1

---
+++ test_check_run_zokeeper.ps1
@@ -0,0 +1,17 @@
+﻿$zookeeper_port = 2181
+$timeout = 5
+
+try {
+    $tcpclient = New-Object System.Net.Sockets.TcpClient
+    $asyncResult = $tcpclient.BeginConnect('NIKITA', $zookeeper_port, $null, $null)
+    $waitHandle = $asyncResult.AsyncWaitHandle
+    if ($waitHandle.WaitOne($timeout * 1000, $false)) {
+        $tcpclient.EndConnect($asyncResult)
+        Write-Output "Zookeeper is running on port $zookeeper_port"
+        $tcpclient.Close()
+    } else {
+        Write-Output "Zookeeper is not running on port $zookeeper_port"
+    }
+} catch {
+    Write-Output "Zookeeper is not running on port $zookeeper_port"
+}
