8d264003116f1886a8d308fe30325de22f95838a
CEMSO18 <csoquet@icloud.com>
2024-07-18 17:36:06 +0200

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cd10f1f3311aa20b2446f2a7cf8ecb03d7734835',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cd10f1f3311aa20b2446f2a7cf8ecb03d7734835',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'opencage/geocode' => array(
+            'pretty_version' => 'v3.3.0',
+            'version' => '3.3.0.0',
+            'reference' => '5e195afccd2769d839f29dc1adca6c1f2c1cb414',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../opencage/geocode',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
