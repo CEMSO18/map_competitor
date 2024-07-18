f6dccba1e67205ba0cb598530b1a5f39c1cce6fd
CEMSO18 <csoquet@icloud.com>
2024-07-18 09:37:29 +0200

    Create check_run_kafka.ps1

---
+++ check_run_kafka.ps1
@@ -0,0 +1,23 @@
+﻿# Vérifiez si Kafka est en cours d'exécution en essayant de se connecter au port Kafka (par défaut 9092)
+function Test-Kafka {
+    param (
+        [string]$server = "localhost",
+        [int]$port = 9092
+    )
+
+    try {
+        $tcpConnection = New-Object System.Net.Sockets.TcpClient($server, $port)
+        $tcpConnection.Close()
+        return $true
+    }
+    catch {
+        return $false
+    }
+}
+
+# Exécute la fonction et affiche le résultat
+if (Test-Kafka) {
+    Write-Host "Kafka est actif" -ForegroundColor Green
+} else {
+    Write-Host "Kafka n'est pas actif" -ForegroundColor Red
+}
