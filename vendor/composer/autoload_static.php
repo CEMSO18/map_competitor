06da07454a5ed8444410f7827cab174b0c715a11
CEMSO18 <csoquet@icloud.com>
2024-07-31 14:40:25 +0200

    mise à jour

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,9 +7,24 @@ namespace Composer\Autoload;
 class ComposerStaticInit2980e7e0c5dbbac3dc2c347ddedfe3e6
 {
     public static $prefixLengthsPsr4 = array (
-        'O' => 
+        'Z' => 
         array (
-            'OpenCage\\Geocoder\\' => 18,
+            'ZipStream\\' => 10,
+        ),
+        'P' => 
+        array (
+            'Psr\\SimpleCache\\' => 16,
+            'Psr\\Http\\Message\\' => 17,
+            'Psr\\Http\\Client\\' => 16,
+            'PhpOffice\\PhpSpreadsheet\\' => 25,
+        ),
+        'M' => 
+        array (
+            'Matrix\\' => 7,
+        ),
+        'C' => 
+        array (
+            'Complex\\' => 8,
         ),
         'A' => 
         array (
@@ -18,9 +33,34 @@ class ComposerStaticInit2980e7e0c5dbbac3dc2c347ddedfe3e6
     );
 
     public static $prefixDirsPsr4 = array (
-        'OpenCage\\Geocoder\\' => 
+        'ZipStream\\' => 
         array (
-            0 => __DIR__ . '/..' . '/opencage/geocode/src',
+            0 => __DIR__ . '/..' . '/maennchen/zipstream-php/src',
+        ),
+        'Psr\\SimpleCache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/simple-cache/src',
+        ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+        ),
+        'Psr\\Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-client/src',
+        ),
+        'PhpOffice\\PhpSpreadsheet\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet',
+        ),
+        'Matrix\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/matrix/classes/src',
+        ),
+        'Complex\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/complex/classes/src',
         ),
         'App\\' => 
         array (
